b77846f5894f52aa9fb9771ab3f8fbff8f880820
ANO-OS <anoosiesus@gmail.com>
2026-03-07 15:00:57 +0800

    changed build.sh

--- ride_share/build.sh
+++ ride_share/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
  
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
